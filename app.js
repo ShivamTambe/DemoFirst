b9a7351b5d851dba9e93a5e1a1368849900469a9
unknown <shivamstambe20222@gmail.com>
2022-08-29 11:08:42 -0700

    add pages

--- app.js
+++ app.js
@@ -15,6 +15,22 @@ app.get("/rewards", function(req, res){
     res.render("rewards");
 })
 
+app.get("/company", function(req, res){
+    res.render("company");
+})
+app.get("/pricing", function(req, res){
+    res.render("pricing");
+})
+app.get("/product", function(req, res){
+    res.render("product");
+})
+app.get("/partners", function(req, res){
+    res.render("partners");
+})
+app.get("/resources", function(req, res){
+    res.render("resources");
+})
+
 app.listen(port, function(){
     console.log("server is running on prot "+ port);
 })
