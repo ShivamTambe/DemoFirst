da22e88c62f5f85cfd308eb128f7647d7dde3939
unknown <shivamstambe20222@gmail.com>
2022-09-01 17:13:42 -0700

    Get started is started

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require ("express");
 const path = require ("path");
 const ejs = require("ejs");
 const app = express();
+const bodyparser = require('body-parser');
 
 const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+
+app.use(bodyparser.urlencoded({extended:false}))
+
 
 app.get("/", function(req, res){
     res.render("index");
@@ -30,7 +34,36 @@ app.get("/partners", function(req, res){
 app.get("/resources", function(req, res){
     res.render("resources");
 })
+app.get("/startbusiness", function(req, res){
+    res.render("startbusiness");
+})
+app.get("/llc", function(req, res){
+    res.render("llc");
+})
 
+app.post("/next",function(req, res){
+    console.log(req.body.radio);
+    let value = req.body.radio;
+
+    if(value == 1 ){
+        console.log("value is 1");
+        res.render("llc");
+    }
+    if(value == 2 ){
+        console.log("value is 2");
+        res.render("llc");
+    }
+    if(value == 3 ){
+        console.log("value is 3");
+        res.render("don'tknow");
+    }
+    if(value == "1-1"){
+        res.render("llc-last");
+    }
+    if(value == "1-2"){
+        res.render("llc-last");
+    }
+})
 app.listen(port, function(){
     console.log("server is running on prot "+ port);
 })
